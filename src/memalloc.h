a0149a877240efd198a5559f13d9d507d0ee8471
Longtao Zhang <DragonBillow@outlook.com>
2022-12-21 17:22:49 +0800

    feat: 禁止名字重整
    Signed-off-by: Longtao Zhang <DragonBillow@outlook.com>

--- src/memalloc.h
+++ src/memalloc.h
@@ -6,9 +6,9 @@ extern "C" {
 #include <stddef.h>
 }
 
-struct header_t;
-
+extern "C" {
 void* mem_alloc(size_t size);
 void  mem_free(void* block);
 void* mem_calloc(size_t num, size_t nsize);
 void* mem_realloc(void* block, size_t size);
+}
